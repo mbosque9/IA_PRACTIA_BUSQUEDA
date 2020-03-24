c12d5775f255642006becc6b9b1f2ec9fc87988b
mbosque9 <mbosque.9@gmail.com>
2020-03-24 16:28:04 +0100

    first commmit

---
+++ src/ServidorsBoard.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ServidorsBoard {
+
+    private ArrayList< ArrayList<Integer>> Board;
+    public ServidorsBoard(){
+            //crear estat inicial
+    }
+
+    public void intercanviar_fitxers( Integer id1, Integer id2){
+
+    }
+
+    public void moure_fitxer(Integer idFitxer, Integer idServidor){
+
+    }
+}
