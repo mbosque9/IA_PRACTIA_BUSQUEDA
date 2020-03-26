b216b98893ff4dd9ad5a4b237d954eb528e7ab78
marina <ladydoofi@yahoo.es>
2020-03-26 13:21:10 +0100

    Marina - Versió 4 (canvi emodificat = new)

--- src/ServersSuccessorFunction.java
+++ src/ServersSuccessorFunction.java
@@ -17,7 +17,7 @@ public class ServersSuccessorFunction {
 
     public ArrayList genera_successors(Object eactual) {
         ServersBoard board = (ServersBoard) eactual;
-        ServersBoard emodificat = new ServersBoard(board.getRequests(), board.getServers());
+        ServersBoard emodificat;
         int sa, s;
         int npeticions = board.getSize();
         ArrayList<Integer> ssp;
@@ -28,7 +28,7 @@ public class ServersSuccessorFunction {
             for (int j = 0; j < ssp.size(); j++) {      //recorro cada servidor que te el fitxer
                 s = ssp.get(j);
                 if (sa != s) {      //comprovo que no siguin el mateix servidor
-                    emodificat = board;
+                    emodificat  = new ServersBoard(board.getRequests(), board.getServers());
                     emodificat.moure_servidor(i,s);       //a la posició i vull posar el nou servidor s
                     successors.add(new Successor(emodificat));      //com transpes pero espera 2 params CERC QUE SHA DE FICAR UN STRING DE QUIN MOVIMENT HAS FET
                 }
@@ -39,7 +39,7 @@ public class ServersSuccessorFunction {
 
     public ArrayList genera_successors_aleatori(Object eactual) {
         ServersBoard board = (ServersBoard) eactual;
-        ServersBoard emodificat = new ServersBoard(board.getRequests(), board.getServers());
+        ServersBoard emodificat;
         boolean generat = false;
         int sa, s, a1, a2;
         int npeticions = board.getSize();
@@ -52,7 +52,7 @@ public class ServersSuccessorFunction {
             a2 = genera_aleatori(ssp.size());
             s = ssp.get(a2);
             if (sa != s) {      //comprovo que no siguin el mateix servidor
-                emodificat = board;
+                emodificat = new ServersBoard(board.getRequests(), board.getServers());
                 emodificat.moure_servidor(a1,s);
                 successors.add(new Successor(emodificat));      //com transpes pero espera 2 params
                 generat = true;
