8deade0260e869cb77dfbc2d2f956aadb07873fe
xaviv <xavier.valls.ruiz@est.fib.upc.edu>
2020-03-28 16:15:55 +0100

    Merge branch 'master' of https://github.com/mbosque9/IA_PRACTIA_BUSQUEDA
     Conflicts:
    	src/ServersSuccessorFunctionHC.java
    	src/ServersSuccessorFunctionSA.java

--- src/ServersSuccessorFunctionHC.java
+++ src/ServersSuccessorFunctionHC.java
@@ -10,8 +10,11 @@ public class ServersSuccessorFunctionHC implements SuccessorFunction {
         ArrayList<Successor> successors = new ArrayList<>();
         ServersBoard board = (ServersBoard) eactual;
         ServersHeuristicFunction Heur = new ServersHeuristicFunction();
+        ServersHeuristicFunction Heur2 = new ServersHeuristicFunction();
+        double h2 = Heur.getHeuristicValue(board);
+        System.out.println("heu value: " + h2);
 
-        int sa, s;
+        Integer sa, s, t;
         int npeticions = board.getSize();
         ArrayList<Integer> ssp;
 
@@ -21,15 +24,17 @@ public class ServersSuccessorFunctionHC implements SuccessorFunction {
             System.out.println("servidor estat: " + sa);
             for (int j = 0; j < ssp.size(); j++) {      //recorro cada servidor que te el fitxer
                 s = ssp.get(j);
+                t = board.getTemps(i, s);
                 System.out.println("servidor posible: " + s);
                 if (sa != s) {      //comprovo que no siguin el mateix servidor
                     System.out.println("entro if ");
                     ServersBoard emodificat = new ServersBoard(board.getBoard(), board.getBoardtemps());
-                    emodificat.moure_servidor(i,s);       //a la posició i vull posar el nou servidor s
+                    emodificat.moure_servidor(i,s, t);       //a la posició i vull posar el nou servidor s
                     for (int a = 0; a < emodificat.getSize(); a++) {
                         System.out.println("La peticio  "+ a + "  te assignat el servidor  " + emodificat.getServidor(a));
                     }
                     double h = Heur.getHeuristicValue(emodificat);
+                    System.out.println("heu value: " + h);
                     String S = ServersBoard.MOURE + " el servidor " + s + " a la petició" + i + " Coste(" + h + ") ----> " + emodificat.toString();
                     successors.add(new Successor(S,emodificat));
                 }
